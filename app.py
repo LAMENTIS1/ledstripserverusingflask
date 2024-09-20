def452213d67b4898d0efc79f864a2ac81b851c7
LAMENTIS1 <124012543+LAMENTIS1@users.noreply.github.com>
2024-09-20 10:48:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/send/<message>', methods=['GET'])
+def send_message(message):
+    return jsonify({'message': message})
+
+if __name__ == '__main__':
+    app.run(debug=True)
